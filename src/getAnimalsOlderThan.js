41e0be85329e0b5e2991d11c71d385ec7ffe7459
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-28 17:22:32 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const especieBuscada = data.species.find((e) => e.name === animal);
+  return especieBuscada.residents.every((e) => e.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
