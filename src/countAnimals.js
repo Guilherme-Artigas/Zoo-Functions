20fc19e870b7dadda9eb62482f2d47b33392ee7c
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-29 12:57:42 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const allSpecies = data.species.reduce((acc, cur) => {
+    const key = cur.name;
+    if (!acc[key]) {
+      acc[key] = cur.residents.length;
+    }
+    return acc;
+  }, {});
+  if (animal === undefined) return allSpecies;
+  const qntChaves = Object.keys(animal);
+  const specie = data.species.find((e) => e.name === animal.specie);
+  if (qntChaves.length === 1) return specie.residents.length;
+  const sexo = specie.residents.filter((e) => e.sex === animal.sex);
+  if (qntChaves.length === 2) return sexo.length;
 }
 
 module.exports = countAnimals;
