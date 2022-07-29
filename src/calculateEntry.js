2c658ba06f6b37b33520aad663a58a01208f1865
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-29 16:42:21 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  const criancas = entrants.filter((e) => e.age < 18);
+  const adultos = entrants.filter((e) => e.age >= 18 && e.age < 50);
+  const idosos = entrants.filter((e) => e.age >= 50);
+  return {
+    child: criancas.length,
+    adult: adultos.length,
+    senior: idosos.length,
+  };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = 0) {
+  const idadeVisitantes = countEntrants(entrants);
+  if (countEntrants(entrants) === 0) return 0;
+  const { adult, child, senior } = idadeVisitantes;
+  return adult * 49.99 + child * 20.99 + senior * 24.99;
 }
 
 module.exports = { calculateEntry, countEntrants };
