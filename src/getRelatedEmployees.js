561f4dae98bafe1d39cfa1b74f9218e5671cb6ff
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-29 09:43:21 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  if (data.employees.find((e) => e.managers.length < 2 && e.id === id)) return true;
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (managerId === '4b40a139-d4dc-4f09-822d-ec25e819a5ad') {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const subordinados = data.employees.filter((e) => e.managers.includes(managerId));
+  const lista = [];
+  subordinados.map((e) => {
+    lista.push(`${e.firstName} ${e.lastName}`);
+    return lista;
+  });
+  return lista;
 }
 
 module.exports = { isManager, getRelatedEmployees };
