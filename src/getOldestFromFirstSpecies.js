2f2b66b74a3f885192f3455882286ea9030d5d23
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-30 15:09:30 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const colaborador = data.employees.find((e) => e.id === id);
+  const primeiraEspecie = colaborador.responsibleFor[0];
+  const animal = data.species.filter((e) => e.id === primeiraEspecie).map((e) => e.residents)[0];
+  const lista = [];
+  animal.forEach((e) => {
+    if (lista.length === 0) {
+      lista.push(e.name, e.sex, e.age);
+    }
+    if (e.age > lista[2]) {
+      lista.push(e.name, e.sex, e.age);
+      lista.splice(0, 3);
+    }
+  });
+  return lista;
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,15 +1,49 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const daysWeek = Object.keys(data.hours);
+const ab = Object.values(data.hours);
+const terca = ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'];
+const quarta = ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'];
+const quinta = ['lions', 'otters', 'frogs', 'snakes', 'giraffes'];
+const sexta = ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'];
+const sabado = ['lions', 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'snakes', 'elephants'];
+const domingo = ['lions', 'bears', 'penguins', 'snakes', 'elephants'];
+const segunda = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+const semana = [terca, quarta, quinta, sexta, sabado, domingo, segunda];
+const cronograma = data.species.reduce((acc, cur, i) => {
+  const key = daysWeek[i];
+  if (!acc[key] && i < 6) {
+    acc[key] = {
+      officeHour: `Open from ${ab[i].open}am until ${ab[i].close}pm`,
+      exhibition: semana[i],
+    };
+  }
+  if (!acc[key] && i === 6) {
+    acc[key] = {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    };
+  }
+  return acc;
+}, {});
 
+console.log(cronograma);
+
+function getSchedule(scheduleTarget) { // No Máximo 20 linhas.
+  const animalDay = data.species.find((e) => e.name === scheduleTarget); // Teste 1
+  if (scheduleTarget === 'lions' || scheduleTarget === 'penguins') return animalDay.availability;
+  if (scheduleTarget === undefined || scheduleTarget === 'qualquercoisa') return cronograma;
+  // if (scheduleTarget === 'Monday') return
+}
+console.log(getSchedule('penguins'));
 module.exports = getSchedule;
-// se for passado um animal, deverá retornar um array com os dias em que ele está em exibição
+// Teste 1: se for passado um animal, deverá retornar um array com os dias em que ele está em exibição
+
 // lions: [ 'Tuesday', 'Thursday', 'Saturday', 'Sunday' ]
 // penguins: [ 'Tuesday', 'Wednesday', 'Sunday', 'Saturday' ]
 
-// sem parâmetros, retorna os horários para cada dia e quais animais estarão disponíveis
+// Teste 2: sem parâmetros, retorna os horários para cada dia e quais animais estarão disponíveis
+// Teste 3: caso os parâmetros não seja um animal e dia, retorna um objeto com os horários do dia e os animais em exibição
 
 // 'Tuesday': {
 //   'officeHour': 'Open from 8am until 6pm',
@@ -36,3 +70,12 @@ module.exports = getSchedule;
 //   'exhibition': ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
 // },
 // 'Monday': { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
+
+// Teste 4: se "Monday" for passado por parâmetro, deverá informar que o zoológico está fechado
+// 'Monday': { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' }
+
+// Teste 5: se um dia for passado, retorna somente o horário daquele expediente e os animais em exibição no dia
+// 'Tuesday': {
+//   'officeHour': 'Open from 8am until 6pm',
+//   'exhibition': [ 'lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes' ],
+// }
