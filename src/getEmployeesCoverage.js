74737bb94fe19ac0d4a28979920cc1c4bef01e9f
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-30 23:19:49 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,52 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const fullName = (name, id) => {
+  const colaborador = data.employees.find((e) => e.firstName === name
+  || e.lastName === name || e.id === id);
+  if (colaborador === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return colaborador;
+};
+
+const buscaLocalizacao = (array) => {
+  const listaLocalizacao = [];
+  data.species.forEach((e) => {
+    if (array.includes(e.name) || array.includes(e.id)) {
+      listaLocalizacao.push(e.location);
+    }
+  });
+  return listaLocalizacao;
+};
+
+const buscaEspecies = (array) => {
+  const listaAnimais = [];
+  data.species.forEach((e) => {
+    if (array.includes(e.id)) {
+      listaAnimais.push(e.name);
+    }
+  });
+  return listaAnimais;
+};
+
+const colaboradores = () => data.employees.map((e) => ({
+  id: e.id,
+  fullName: `${e.firstName} ${e.lastName}`,
+  species: buscaEspecies(e.responsibleFor),
+  locations: buscaLocalizacao(e.responsibleFor),
+}));
+
+function getEmployeesCoverage(obj) {
+  if (obj === undefined) return colaboradores();
+  const nomeCompleto = fullName(obj.name, obj.id);
+  const especies = buscaEspecies(nomeCompleto.responsibleFor);
+  const localizacao = buscaLocalizacao(especies);
+  return {
+    id: nomeCompleto.id,
+    fullName: `${nomeCompleto.firstName} ${nomeCompleto.lastName}`,
+    species: especies,
+    locations: localizacao,
+  };
 }
 
 module.exports = getEmployeesCoverage;
