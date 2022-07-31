aa72db03ef0cec8ef7d0a20637bb75925cf18135
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-31 13:21:43 -0300

    Requisitos 12 e 13

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,43 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  test('Caso não receba nenhum valor deve retornar todos os dias e horarios', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+
+  test('Caso a função receba os valores, Monday e 09:00-AM', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+
+  test('Caso a função receba os valores, Wednesday e 09:00-PM', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+
+  test('Caso a função receba os valores, Tuesday e 09:00-AM', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+
+  test('Caso receba, Thu e 09:00-AM', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow(/^The day must be valid. Example: Monday$/);
+  });
+
+  test('Caso receba, Friday e 09:00-ZM', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+
+  test('Caso receba, Saturday e C9:00-AM', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow(/^The hour should represent a number$/);
+  });
+
+  test('Caso receba, Sunday e 09:c0-AM', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow(/^The minutes should represent a number$/);
+  });
+});
